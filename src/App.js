b7b58eb7122d23d9cc40eec3fe072ded8e933813
plamen1982 <plamen.hristov@ymail.com>
2019-10-07 15:28:31 +0300

    reusable-card-created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import StarWarsCard from '../src/components/presentational/StarWarsCard';
 
 function App() {
+  const styles = {
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">Test</header>
+    <div className="App" style={styles.container}>
+      <StarWarsCard />
+      <StarWarsCard />
+      <StarWarsCard />
     </div>
   );
 }
