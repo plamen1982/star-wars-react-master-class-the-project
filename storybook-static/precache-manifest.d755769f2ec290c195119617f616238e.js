347663e1e0c0c6a414e94b973ba88ddc98b57272
plamen1982 <plamen.hristov@ymail.com>
2019-10-07 13:27:06 +0300

    build files, add data.json

---
+++ storybook-static/precache-manifest.d755769f2ec290c195119617f616238e.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdeadcf99ea921c458ff283b9702e72e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bbafe2bb658c8903ba0f.bundle.js"
+  },
+  {
+    "url": "runtime~main.bbafe2bb658c8903ba0f.bundle.js"
+  },
+  {
+    "url": "vendors~main.bbafe2bb658c8903ba0f.bundle.js"
+  }
+]);
