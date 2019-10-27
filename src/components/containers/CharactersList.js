ebb738d53de3549308eb90b5bdccd5887b82ef0d
plamen1982 <plamen.hristov@ymail.com>
2019-10-27 20:11:42 +0200

    syntax error removed

--- src/components/containers/CharactersList.js
+++ src/components/containers/CharactersList.js
@@ -37,7 +37,7 @@ export default function EpisodesList() {
       return <div>...Ops you have errors, message: {props.error.message}</div>;
     },
     onCompleted: props => {
-      if (!props.appPeople) {
+      if (!props.allPeople) {
         window.localStorage.setItem('token', '');
         history.push('/login');
       }
