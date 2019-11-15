ed86cdfc126aadb4faa5aaaaadc23a725241fc9b
plamen1982 <plamen.hristov@ymail.com>
2019-11-15 19:01:10 +0200

    US-20: added global mockup for AppoloProvider and ThemeProvider with react testing library

---
+++ src/test-utils/index.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import { ThemeContext } from '../contexts';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+const AllProvider = ({ children }) => {
+  const currentThemeMock = { colors: 'mocked colors theme' };
+  const toggleThemeMock = () => 'mocked toggle theme';
+  const mockedApolloClient = new ApolloClient(); // maybe Apollo is going to ask for some configs
+  return (
+    <ThemeContext.Provider value={{ currentThemeMock, toggleThemeMock }}>
+      <ApolloProvider client={mockedApolloClient}>{children}</ApolloProvider>
+    </ThemeContext.Provider>
+  );
+};
+//TODO
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllProvider, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+
+export { customRender as render };
